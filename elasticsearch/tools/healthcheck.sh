11b8de8434fa3370d95674d5707245d6c49646e0
jlefonde <joris.lefondeur@protonmail.com>
2024-11-29 20:57:37 +0100

    Improved logstash.yml

--- elasticsearch/tools/healthcheck.sh
+++ elasticsearch/tools/healthcheck.sh
@@ -2,4 +2,4 @@
 
 ELASTIC_PASSWORD=$(cat $ELASTIC_PASSWORD_FILE)
 
-curl -s -f -u elastic:"$ELASTIC_PASSWORD" http://localhost:9200/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
+curl -s -f -u elastic:"$ELASTIC_PASSWORD" http://localhost:$ELASTIC_PORT/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
