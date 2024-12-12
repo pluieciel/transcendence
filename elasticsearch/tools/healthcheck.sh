d7be507fb231dcffab0d20439607e4b83953cc98
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-12 11:19:50 +0100

    Added https to elasticsearch

--- elasticsearch/tools/healthcheck.sh
+++ elasticsearch/tools/healthcheck.sh
@@ -2,4 +2,7 @@
 
 ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 
-curl -s -f -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" http://localhost:$ELASTIC_PORT/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
+curl -s -f --cacert /usr/share/elasticsearch/config/elastic-certificates.pem \
+    --resolve instance:9200:127.0.0.1 \
+    -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
+    "https://localhost:$ELASTIC_PORT/_cluster/health?pretty" | grep status | grep -q '\(green\|yellow\)'
