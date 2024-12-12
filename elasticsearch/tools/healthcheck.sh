0f6626fa8c1c63d040e65b63c4aa07f6c6c6c92f
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-12 13:14:08 +0100

    Fixed elasticsearch healthcheck

--- elasticsearch/tools/healthcheck.sh
+++ elasticsearch/tools/healthcheck.sh
@@ -2,7 +2,7 @@
 
 ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 
-curl -s -f --cacert /usr/share/elasticsearch/config/elastic-certificates.pem \
-    --resolve instance:9200:127.0.0.1 \
-    -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
-    "https://localhost:$ELASTIC_PORT/_cluster/health?pretty" | grep status | grep -q '\(green\|yellow\)'
+curl -s -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
+	--cacert config/elastic-certificates.pem \
+    --resolve instance:$ELASTIC_PORT:127.0.0.1 \
+    https://instance:$ELASTIC_PORT/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
