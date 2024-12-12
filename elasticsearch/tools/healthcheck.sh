6b06135ca336716a295f982773fe58fff9a95994
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-12 14:35:39 +0100

    Modified healthcheck

--- elasticsearch/tools/healthcheck.sh
+++ elasticsearch/tools/healthcheck.sh
@@ -4,5 +4,4 @@ ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 
 curl -s -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
 	--cacert config/elastic-certificates.pem \
-    --resolve instance:$ELASTIC_PORT:127.0.0.1 \
-    https://instance:$ELASTIC_PORT/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
+    $ELASTIC_HOST/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
--- elasticsearch/tools/init_elasticsearch.sh
+++ elasticsearch/tools/init_elasticsearch.sh
@@ -5,10 +5,12 @@ export ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 if [ ! -f ./config/elastic-certificates.p12 ]; then
     ./bin/elasticsearch-certutil ca --pass "$ELASTIC_PASSWORD" --out ./config/elastic-stack-ca.p12
 
-    ./bin/elasticsearch-certutil cert --ca ./config/elastic-stack-ca.p12 \
+    ./bin/elasticsearch-certutil cert \
+        --ca ./config/elastic-stack-ca.p12 \
         --ca-pass "$ELASTIC_PASSWORD" \
         --pass "$ELASTIC_PASSWORD" \
-        --out ./config/elastic-certificates.p12
+        --out ./config/elastic-certificates.p12 \
+        --dns elasticsearch
 
     ./bin/elasticsearch-keystore create
 
@@ -18,7 +20,10 @@ if [ ! -f ./config/elastic-certificates.p12 ]; then
     ./bin/elasticsearch-keystore add -f xpack.security.http.ssl.keystore.secure_password <<< "$ELASTIC_PASSWORD"
     ./bin/elasticsearch-keystore add -f xpack.security.http.ssl.truststore.secure_password <<< "$ELASTIC_PASSWORD"
 
-    openssl pkcs12 -in ./config/elastic-certificates.p12 -out ./config/elastic-certificates.pem -nokeys -passin pass:"$ELASTIC_PASSWORD"
+    openssl pkcs12 -in ./config/elastic-certificates.p12 \
+        -out ./config/elastic-certificates.pem \
+        -nokeys \
+        -passin pass:"$ELASTIC_PASSWORD"
 fi
 
 exec /bin/tini -- /usr/local/bin/docker-entrypoint.sh
