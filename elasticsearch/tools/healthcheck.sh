27d4bdf27472c6a9f2f4a3d0100be8feaa9515cf
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-13 08:39:23 +0100

    Revert "Modified healthcheck"
    This reverts commit 6b06135ca336716a295f982773fe58fff9a95994.

--- elasticsearch/tools/healthcheck.sh
+++ elasticsearch/tools/healthcheck.sh
@@ -4,4 +4,5 @@ ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 
 curl -s -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
 	--cacert config/elastic-certificates.pem \
-    $ELASTIC_HOST/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
+    --resolve instance:$ELASTIC_PORT:127.0.0.1 \
+    https://instance:$ELASTIC_PORT/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
--- elasticsearch/tools/init_elasticsearch.sh
+++ elasticsearch/tools/init_elasticsearch.sh
@@ -5,12 +5,10 @@ export ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 if [ ! -f ./config/elastic-certificates.p12 ]; then
     ./bin/elasticsearch-certutil ca --pass "$ELASTIC_PASSWORD" --out ./config/elastic-stack-ca.p12
 
-    ./bin/elasticsearch-certutil cert \
-        --ca ./config/elastic-stack-ca.p12 \
+    ./bin/elasticsearch-certutil cert --ca ./config/elastic-stack-ca.p12 \
         --ca-pass "$ELASTIC_PASSWORD" \
         --pass "$ELASTIC_PASSWORD" \
-        --out ./config/elastic-certificates.p12 \
-        --dns elasticsearch
+        --out ./config/elastic-certificates.p12
 
     ./bin/elasticsearch-keystore create
 
@@ -20,10 +18,7 @@ if [ ! -f ./config/elastic-certificates.p12 ]; then
     ./bin/elasticsearch-keystore add -f xpack.security.http.ssl.keystore.secure_password <<< "$ELASTIC_PASSWORD"
     ./bin/elasticsearch-keystore add -f xpack.security.http.ssl.truststore.secure_password <<< "$ELASTIC_PASSWORD"
 
-    openssl pkcs12 -in ./config/elastic-certificates.p12 \
-        -out ./config/elastic-certificates.pem \
-        -nokeys \
-        -passin pass:"$ELASTIC_PASSWORD"
+    openssl pkcs12 -in ./config/elastic-certificates.p12 -out ./config/elastic-certificates.pem -nokeys -passin pass:"$ELASTIC_PASSWORD"
 fi
 
 exec /bin/tini -- /usr/local/bin/docker-entrypoint.sh
