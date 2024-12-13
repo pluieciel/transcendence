58c534b9027b81e5f86a2161dbb6c20bc0677b36
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-13 13:47:23 +0100

    Removed pong folder

--- pong/django/Game/Gamerouting.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from Game.consumer import GameConsumer
-
-# the empty string routes to ChatConsumer, which manages the chat functionality.
-websocket_urlpatterns = [
-    path("game/", GameConsumer.as_asgi()),
-]
