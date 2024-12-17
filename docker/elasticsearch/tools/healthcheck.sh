5e785168ebd5e805ad2640d6bad3cd967fadbc6a
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-17 12:53:45 +0100

    Added elasticsearch to nginx reverse proxy

--- docker/elasticsearch/tools/healthcheck.sh
+++ docker/elasticsearch/tools/healthcheck.sh
@@ -3,5 +3,4 @@
 ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 
 curl -s -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
-	--cacert ./config/certs/elastic-certificates.pem \
     $ELASTIC_HOST/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
