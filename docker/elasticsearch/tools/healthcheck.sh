6ed8d111ceb5862d1b5a1b2f4d306ad05a020708
Joris Lefondeur <jlefonde@c1r3s1.42luxembourg.lu>
2024-12-19 09:27:09 +0100

    Fixed kibana config

--- docker/elasticsearch/tools/healthcheck.sh
+++ docker/elasticsearch/tools/healthcheck.sh
@@ -3,4 +3,4 @@
 ELASTIC_PASSWORD=$(cat $ELASTICSEARCH_PASSWORD_FILE)
 
 curl -s -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" \
-    $ELASTIC_HOST/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
+    http://localhost:9200/_cluster/health?pretty | grep status | grep -q '\(green\|yellow\)'
