a3bf1e0e0b4f549da088c781e2136f3da2d1c9f2
jlefonde <joris.lefondeur@protonmail.com>
2025-01-17 16:10:22 +0100

    Added jwk file

--- docker/nginx/tools/init_nginx.sh
+++ docker/nginx/tools/init_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /etc/nginx/certs/nginx.key ] || [ ! -f /etc/nginx/certs/nginx.crt ]; then
-    openssl genrsa -out /etc/nginx/certs/nginx.key 2048
+	openssl genrsa -out /etc/nginx/certs/nginx.key 2048
 
     openssl req -new \
         -key /etc/nginx/certs/nginx.key \
@@ -13,6 +13,24 @@ if [ ! -f /etc/nginx/certs/nginx.key ] || [ ! -f /etc/nginx/certs/nginx.crt ]; t
         -keyout /etc/nginx/certs/nginx.key \
         -out /etc/nginx/certs/nginx.crt \
         -config /etc/nginx/openssl.conf
+fi
+
+if [ ! -f /etc/nginx/certs/jwt.jwk ]; then
+    JWT_SECRET=$(cat "$JWT_SECRET_KEY_FILE")
+    JWT_SECRET_B64=$(echo -n "$JWT_SECRET" | base64 | tr -d '\n')
+
+    cat > /etc/nginx/certs/jwt.jwk << EOF
+{
+    "keys": [
+        {
+            "k": "$JWT_SECRET_B64",
+            "kty": "oct",
+            "kid": "0001",
+            "alg": "HS256"
+        }
+    ]
+}
+EOF
 fi
 
 exec /usr/sbin/nginx -g "daemon off;"
