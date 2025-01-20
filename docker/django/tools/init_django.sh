4396f08c48da0bbd5a4361d2cd3dae725f1e0bae
jlefonde <joris.lefondeur@protonmail.com>
2025-01-20 12:56:56 +0100

    Removed django static

--- docker/django/tools/init_django.sh
+++ docker/django/tools/init_django.sh
@@ -6,7 +6,6 @@ export JWT_SECRET_KEY=$(cat $JWT_SECRET_KEY_FILE)
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
 
 if ! python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists()" | grep -q 'True'; then
         python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME
@@ -15,4 +14,3 @@ else
 fi
 
 exec daphne -b 0.0.0.0 -p 8000 backend.asgi:application
-#exec python manage.py runserver 0.0.0.0:8000 not working for chat and signup
