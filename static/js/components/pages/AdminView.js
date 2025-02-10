56ed754c4fabc94346a19bf1e5d0da7369544e4d
jlefonde <joris.lefondeur@protonmail.com>
2025-02-10 13:23:52 +0100

    Fixed admin view

--- static/js/components/pages/AdminView.js
+++ static/js/components/pages/AdminView.js
@@ -26,35 +26,35 @@ export default class AdminView {
 							<p class="service-description">Search and analytics engine for all application logs and metrics.</p>
 							<button id="elasticsearch-button" class="service-button">Go to Elasticsearch</button>
 						</div>
-						<div id="service-card" class="card">
+						<div class="card service-card">
 							<h2 id="card-title">
 								<img src="/imgs/kibana_logo.webp" alt="Kibana logo" class="service-logo"> KIBANA
 							</h2>
 							<p class="service-description">Visualization dashboard for Elasticsearch data and log analysis.</p>
 							<button id="kibana-button" class="service-button">Go to Kibana</button>
 						</div>
-						<div id="service-card" class="card">
+						<div class="card service-card">
 							<h2 id="card-title">
 								<img src="/imgs/cadvisor_logo.webp" alt="cAdvisor logo" class="service-logo"> CADVISOR
 							</h2>
 							<p class="service-description">Container resource usage and performance analyzer.</p>
 							<button id="cadvisor-button" class="service-button">Go to cAdvisor</button>
 						</div>
-						<div id="service-card" class="card">
+						<div class="card service-card">
 							<h2 id="card-title">
 								<i class="fa-solid fa-file-export fa-xs"></i> NODE EXPORTER
 							</h2>
 							<p class="service-description">Hardware and OS metrics exporter for system monitoring.</p>
 							<button id="node-exporter-button" class="service-button">Go to Node Exporter</button>
 						</div>
-						<div id="service-card" class="card">
+						<div class="card service-card">
 							<h2 id="card-title">
 								<img src="/imgs/prometheus_logo.webp" alt="Prometheus logo" class="service-logo"> PROMETHEUS
 							</h2>
 							<p class="service-description">Time series database for metrics collection and alerting.</p>
 							<button id="prometheus-button" class="service-button">Go to Prometheus</button>
 						</div>
-						<div id="service-card" class="card">
+						<div class="card service-card">
 							<h2 id="card-title">
 								<img src="/imgs/grafana_logo.webp" alt="Grafana logo" class="service-logo"> GRAFANA
 							</h2>
