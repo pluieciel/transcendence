98d51612d4a3c1c66f575cfb2947d2c3fff053c0
jlefonde <joris.lefondeur@protonmail.com>
2025-02-10 14:53:40 +0100

    Fixed admin view api

--- static/js/components/pages/AdminView.js
+++ static/js/components/pages/AdminView.js
@@ -17,52 +17,7 @@ export default class AdminView {
 			if (data.success) {
 				await window.app.getSettings();
 				window.app.renderHeader(this.container, "admin");
-				this.container.innerHTML += `
-					<main id="admin-view">
-						<div class="card service-card">
-							<h2 id="card-title">
-								<img src="/imgs/elasticsearch_logo.webp" alt="Elasticsearch logo" class="service-logo"> ELASTICSEARCH
-							</h2>
-							<p class="service-description">Search and analytics engine for all application logs and metrics.</p>
-							<button id="elasticsearch-button" class="service-button">Go to Elasticsearch</button>
-						</div>
-						<div class="card service-card">
-							<h2 id="card-title">
-								<img src="/imgs/kibana_logo.webp" alt="Kibana logo" class="service-logo"> KIBANA
-							</h2>
-							<p class="service-description">Visualization dashboard for Elasticsearch data and log analysis.</p>
-							<button id="kibana-button" class="service-button">Go to Kibana</button>
-						</div>
-						<div class="card service-card">
-							<h2 id="card-title">
-								<img src="/imgs/cadvisor_logo.webp" alt="cAdvisor logo" class="service-logo"> CADVISOR
-							</h2>
-							<p class="service-description">Container resource usage and performance analyzer.</p>
-							<button id="cadvisor-button" class="service-button">Go to cAdvisor</button>
-						</div>
-						<div class="card service-card">
-							<h2 id="card-title">
-								<i class="fa-solid fa-file-export fa-xs"></i> NODE EXPORTER
-							</h2>
-							<p class="service-description">Hardware and OS metrics exporter for system monitoring.</p>
-							<button id="node-exporter-button" class="service-button">Go to Node Exporter</button>
-						</div>
-						<div class="card service-card">
-							<h2 id="card-title">
-								<img src="/imgs/prometheus_logo.webp" alt="Prometheus logo" class="service-logo"> PROMETHEUS
-							</h2>
-							<p class="service-description">Time series database for metrics collection and alerting.</p>
-							<button id="prometheus-button" class="service-button">Go to Prometheus</button>
-						</div>
-						<div class="card service-card">
-							<h2 id="card-title">
-								<img src="/imgs/grafana_logo.webp" alt="Grafana logo" class="service-logo"> GRAFANA
-							</h2>
-							<p class="service-description">Metrics visualization and monitoring dashboard platform.</p>
-							<button id="grafana-button" class="service-button">Go to Grafana</button>
-						</div>
-					</main>
-				`;
+				this.container.innerHTML += data.admin_view;
 				window.app.checkForAdmin();
 				this.addEventListeners();
 			} else if (response.status == 409) {
