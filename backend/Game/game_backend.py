f911b886dc8a8f69971bcbdd3bee5a7720169a85
Julien Nunes <jnunes@c1r3s2.42luxembourg.lu>
2025-02-24 17:06:25 +0100

    added more info on game end for new end modal

--- backend/Game/game_backend.py
+++ backend/Game/game_backend.py
@@ -357,6 +357,20 @@ class GameBackend:
 		except Exception as e:
 			self.logger.info(f"Error {e}")
 
+	def getUserAvatar(self, user):
+		if (user.avatar_42):
+			return user.avatar_42
+		elif (user.avatar):
+			return user.avatar.url
+		else:
+			return '/imgs/default_avatar.png'
+	
+	def getUserName(self, user):
+		if (user.display_name):
+			return user.display_name
+		else:
+			return user.username
+
 	async def rumble_broadcast_state(self):
 		events = []
 		if self.game.scored:
@@ -381,24 +395,26 @@ class GameBackend:
 			})
 			self.game.scored = False
 		if self.game.ended:
-			if (self.game.winner.avatar_42):
-				avatar = self.game.winner.avatar_42
-			elif (self.game.winner.avatar):
-				avatar = self.game.winner.avatar.url
+			avatar = self.getUserAvatar(self.game.winner)
+			username = self.getUserName(self.game.winner)
+			if self.game.winner is self.player_left.user:
+				side = "LEFT"
+			elif self.game.winner is self.player_right.user:
+				side = "RIGHT"
 			else:
-				avatar = '/imgs/default_avatar.png'
+				self.logger.info("Unknown winner")
 
-			self.logger.info(f"Appending winner info with {self.game.winner.username}")
-			if (self.game.winner.display_name):
-				username = self.game.winner.display_name
-			else:
-				username = self.game.winner.username
 			events.append({
 				"type": "game_end",
 				"winnerName": username,
+				"winner" : side,
 				"winnerUser" : self.game.winner.username,
 				"winnerAvatar": avatar,
+				"playerLeftName" : self.getUserName(self.player_left.user),
+				"playerLeftAvatar" : self.getUserAvatar(self.player_left.user),
 				"scoreLeft": self.game.player_left.score,
+				"playerRightName" : self.getUserName(self.player_right.user),
+				"playerRightAvatar" : self.getUserAvatar(self.player_right.user),
 				"scoreRight": self.game.player_right.score,
 				"eloChange": self.elo_change,
 				"tournament": self.tournament
